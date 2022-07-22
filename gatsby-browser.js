7d848017a7e926372b497103dfbc4e267756dd06
Juwon-Yun <eooll2@daum.net>
2022-07-22 11:21:30 +0900

    modify: gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,1 @@
-export const onServiceWorkerUpdateFound = () => {
-    if (
-        window.confirm(
-        `dev-juwon.app has been updated since your last visit.
-        Please press "OK" for update to the latest version`
-        )
-      ) {
-        window.location.reload(true)
-      }
-  }
-
-//   exports.onServiceWorkerUpdateFound = () => window.location.reload(true);
+export const onServiceWorkerUpdateFound = () => window.location.reload(true);
