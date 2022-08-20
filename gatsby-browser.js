149f34496b6697baaa37d133f60c5cfaced76a9c
Juwon-Yun <eooll2@daum.net>
2022-08-20 19:55:35 +0900

    modify: gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
-export const onServiceWorkerUpdateFound = () => window.location.reload(true);
+import { ServiceWorkerArgs } from "gatsby"
+
+export const onServiceWorkerUpdateReady = async (args) => {
+  const permissionResponse = await Notification.requestPermission()
+  if (permissionResponse === "granted") {
+    await args.serviceWorker.showNotification("Website update", {
+      body:
+        "Our website just got a little bit better. We reloaded the site with the update to ensure a smooth experience for you."
+    })
+  }
+  window.location.reload(true)
+}
+
+// export const onServiceWorkerUpdateFound = () => window.location.reload(true);
